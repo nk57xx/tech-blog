c11fe3f2da0c5b2d8d12e0577b741b0d84179c03
nk57xx <norbert.kerner@gmail.com>
2024-11-14 10:53:24 +0100

    added cloudtalents certificate

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,7 +136,7 @@ resource "aws_cloudfront_distribution" "static-website" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.my_certificate.arn
+    acm_certificate_arn      = aws_acm_certificate.my_certificate.id
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
     #cloudfront_default_certificate = true
